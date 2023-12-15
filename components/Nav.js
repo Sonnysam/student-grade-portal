cc2f5276a98e932e8158366c5e678dc0beb7fa2a
sonnysam <samuelagbenyo067@gmail.com>
2023-12-15 15:49:17 +0000

    landing page complete

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,16 @@
-import React from "react";
+
 
 const Nav = () => {
   return (
     <div>
-      <nav className="px-5 py-3 flex justify-between text-white bg-slate-900 shadow-sm">
+      <nav className="px-5 py-3 flex justify-between text-white  shadow-sm">
         <h1 className="text-base cursor-pointer font-extrabold flex items-center lg:text-3xl md:text-2xl sm:text-xl">
           MGRS
         </h1>
 
         <ul className="flex items-center">
           <li className="mx-3 cursor-pointer">Help</li>
-          <li className="mx-3 px-3 py-2 bg-white text-black rounded-md cursor-pointer">Login</li>
+          <li className="mx-3 px-4 py-2 bg-white text-black rounded-md cursor-pointer">Login</li>
         </ul>
       </nav>
     </div>
